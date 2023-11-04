0b7d4ea282db05b33e9dfa79e2a754dc9ab05a9a
Rozakul Barkhi <rozakulbarkhirb@gmail.com>
2023-11-04 20:51:36 +0700

    solution the date object

---
+++ dateObject.js
@@ -0,0 +1,22 @@
+function main() {
+  var year = parseInt(readLine(), 10);
+  var month = parseInt(readLine(), 10);
+  var day = parseInt(readLine(), 10);
+
+  console.log(getWeekDay(year, month, day));
+}
+
+function getWeekDay(year, month, day) {
+  var names = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  var d = new Date(year, month, day);
+  //complete the function
+  return names[d.getDay()];
+}
